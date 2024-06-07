76b59ab846938b03e89c2b0090e4943cca1ba185
Vítor Gabriel <vggd@cin.ufpe.br>
2024-06-06 22:51:36 -0300

    feat 3-6: crud base OK
    fiz a base do crud e vou testar com o front
    aproveitei e ajustei os modulos

--- backend/src/membros/membros.controller.ts
+++ backend/src/membros/membros.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { MembrosService } from './membros.service';
+import { Membro } from './membro.entity';
 
 @Controller('membros')
-export class MembrosController {}
+export class MembrosController {
+    constructor(private readonly membrosService: MembrosService) {}
+
+    @Get('')
+    findAll(): Promise <Membro[]> {
+        return this.membrosService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<Membro> {
+      return this.membrosService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() membro:Membro): Promise<Membro>{
+        return this.membrosService.create(membro);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() membro: Membro): Promise<Membro> {
+        return this.membrosService.update(id, membro);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number): Promise<void> {
+        return this.membrosService.remove(id);
+    }
+}
--- backend/src/membros/membros.service.ts
+++ backend/src/membros/membros.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult } from 'typeorm';
+import { Membro } from './membro.entity';
 
 @Injectable()
-export class MembrosService {}
+export class MembrosService {
+    constructor (
+        @InjectRepository(Membro)
+        private readonly membrosRepository: Repository<Membro>,
+    ) {}
+
+    findAll(): Promise<Membro[]> {
+        return this.membrosRepository.find();
+    }
+
+    findOne(id: number): Promise<Membro | undefined>  {
+        return this.membrosRepository.findOne({ where: { id } });
+    }
+    
+    create(membro: Membro): Promise<Membro> {
+        return this.membrosRepository.save(membro);
+    }
+
+    async update(id: number, membro: Membro): Promise<Membro> {
+        await this.membrosRepository.update(id, membro);
+        return this.findOne(id);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.membrosRepository.delete(id);
+    }
+}
